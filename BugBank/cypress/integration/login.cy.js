76bea13dbda72da6337a615b21a03c44f4ac7221
Walterharder <walter.harder@hotmail.com>
2024-06-24 21:07:55 -0300

    Atualização commands e finalização dos cenários de login

--- BugBank/cypress/integration/login.cy.js
+++ BugBank/cypress/integration/login.cy.js
@@ -6,7 +6,7 @@ describe ('Login', () =>{
         cy.acessoUrl()
     })
 
-    it.only('Validar criação de conta com saldo', () => {
+    it('Validar criação de conta com saldo', () => {
         cy.cadastroComSaldo()
 
         cy.get('input[name="email"]').eq(0).type('teste@automacao.com', {force: true})
@@ -22,34 +22,77 @@ describe ('Login', () =>{
         cy.get('#textAccountNumber').should('contain', 'Conta digital:') 
         cy.get('#textBalance').contains('R$ 1.000,00') 
             
-        // // })
-    
-
-
+        // // })  
     })
 
     it('Validar criação de conta sem saldo', () => {
+        cy.cadastroSemSaldo()
+
+        cy.get('input[name="email"]').eq(0).type('teste@automacao.com', {force: true})
+        cy.get('input[name="password"]').eq(0).type('1234', {force: true})
+        cy.contains('Acessar').click()
+
+        // cy.get('#textAccountNumber > span').invoke('text').then((frase) => {
+        //     frase.match(numero)[0];
+        //     cy.data = numero
+        // })
+
+        cy.get('#textAccountNumber').should('contain', 'Conta digital:') 
+        cy.get('#textBalance').contains('R$ 0,00')
+
+        
 
     })
 
     it('Validar campos obrigatórios de Login da conta', () => {
+        cy.contains('Acessar').click()
+        cy.get('p:contains("obrigatório")').eq(0).should('have.text', 'É campo obrigatório')
+        cy.get('p:contains("obrigatório")').eq(1).should('have.text', 'É campo obrigatório')
+        cy.get('input[name="email"]').eq(0).type('teste@automacao.com', {force: true})
+        cy.contains('Acessar').click()
+        cy.get('p:contains("obrigatório")').eq(0).should('have.text', 'É campo obrigatório')
+        cy.get('input[name="email"]').eq(0).clear().should('have.text', '')
+        cy.get('input[name="password"]').eq(0).type('1234', {force: true})
+        cy.contains('Acessar').click()
+        cy.get('p:contains("obrigatório")').eq(0).should('have.text', 'É campo obrigatório')
 
     })
 
-    it('Validar a tentaiva de login com usuário não cadastrado', () => {
+    it('Validar a tentativa de login de usuário não cadastrado', () => {
+        cy.get('input[name="email"]').eq(0).type('testeauromacao@automacao.com', {force: true})
+        cy.get('input[name="password"]').eq(0).type('123456', {force: true})
+        cy.contains('Acessar').click()
+        
+        cy.get('.styles__ContainerContent-sc-8zteav-1').should('be.visible')
+        cy.get('#modalText').contains('Usuário ou senha inválido. Tente novamente ou verifique suas informações!')
+        cy.get('#btnCloseModal').click()
+    })
+
+    it('Validar a tentativa de login sem usuário', () => {
+        cy.get('input[name="password"]').eq(0).type('123456', {force: true})
+        cy.contains('Acessar').click()
+        cy.get('p:contains("obrigatório")').eq(0).should('have.text', 'É campo obrigatório')
 
     })
 
-    it('Validar a tentaiva de login sem usuário', () => {
-
+    it('Validar a tentativa de login sem a senha', () => {
+        cy.get('input[name="email"]').eq(0).type('testeauromacao@automacao.com', {force: true})
+        cy.contains('Acessar').click()
+        cy.get('p:contains("obrigatório")').eq(0).should('have.text', 'É campo obrigatório')
     })
 
-    it('Validar a tentaiva de login com usuário sem senha', () => {
+    it('Validar a tentativa de login com senha inválida', () => {
+        cy.cadastroComSaldo()
 
-    })
+        cy.get('input[name="email"]').eq(0).type('teste@automacao.com', {force: true})
+        cy.get('input[name="password"]').eq(0).type('12345', {force: true})
+        cy.contains('Acessar').click()
 
-    it('Validar a tentaiva de login com senha inválida', () => {
+        cy.get('.styles__ContainerContent-sc-8zteav-1').should('be.visible')
+        cy.get('#modalText').contains('Usuário ou senha inválido. Tente novamente ou verifique suas informações!')
+        cy.get('#btnCloseModal').click()
 
+  
     })
 
 })
--- BugBank/cypress/support/commands-cadastro.js
+++ BugBank/cypress/support/commands-cadastro.js
@@ -1,29 +1,34 @@
 import { regexp } from "assert-plus"
+import { get } from "lodash"
 
 Cypress.Commands.add('cadastroSemSaldo', function() {
     cy.get('.ihdmxA').click()
-    cy.contains('Cadastrar').should('be.enabled', {timeout:1000}).click({force: true})
+    cy.contains('Cadastrar').click({force: true})
     cy.get('input[name="email"]').eq(1).type('teste@automacao.com', {force: true})
-    cy.get('input[type="name"]').click({force: true}).should('be.enabled').type('Teste Automação')
+    cy.get('input[type="name"]').click({force: true}).type('Teste Automação')
     cy.get('input[name="password"]').eq(1).type('1234', {force: true})
     cy.get('input[name="passwordConfirmation"]').type('1234', {force: true})
     cy.contains('Cadastrar').click({force: true})
-    cy.get('.styles__ContainerContent-sc-8zteav-1').should('be.visible')
-    cy.get('#modalText').contains('foi criada com sucesso')
-    cy.get('#modalText').invoke('text').then((frase) => {
-        const numero = frase.match(/\d+-?\d+/)[0];
-        cy.data = numero
-        cy.log(numero)
+
+    cy.get('.styles__ContainerContent-sc-8zteav-1').should('be.visible');
+    cy.get('#modalText').contains('foi criada com sucesso').then(() => {
+        cy.get('#modalText').invoke('text').then((frase) => {
+            const regex = /\d+-?\d+/;
+            const numero = frase.match(regex)[0];
+            cy.log(numero)
+        })
+        
     })
-    
+   
     cy.get('#btnCloseModal').click()
 })
 
+
 Cypress.Commands.add('cadastroComSaldo', function() {
     cy.get('.ihdmxA').click()
-    cy.contains('Cadastrar').should('be.enabled', {timeout:1000}).click({force: true})
+    cy.contains('Cadastrar').click({force: true})
     cy.get('input[name="email"]').eq(1).type('teste@automacao.com', {force: true})
-    cy.get('input[type="name"]').click({force: true}).should('be.enabled').type('Teste Automação')
+    cy.get('input[type="name"]').click({force: true}).type('Teste Automação')
     cy.get('input[name="password"]').eq(1).type('1234', {force: true})
     cy.get('input[name="passwordConfirmation"]').type('1234', {force: true})
     cy.get('#toggleAddBalance').click({force: true})
@@ -37,62 +42,4 @@ Cypress.Commands.add('cadastroComSaldo', function() {
     })
     
     cy.get('#btnCloseModal').click()
-})
-
-Cypress.Commands.add('cadastroCamposObrigatorios', function() {
-    cy.get('.ihdmxA').click()
-        cy.contains('Cadastrar').should('be.enabled', {timeout:1000}).click({force: true})
-        cy.get('p:contains("obrigatório")').eq(0).should('have.text', 'É campo obrigatório') //
-        cy.get('input[name="email"]').eq(1).type('teste@automacao.com')
-        cy.contains('Cadastrar').click({force: true})
-        cy.get('p:contains("obrigatório")').eq(0).should('have.text', 'É campo obrigatório')
-        cy.get('input[name="password"]').eq(1).type('1234')
-        cy.contains('Cadastrar').click({force: true})
-        cy.get('p:contains("obrigatório")').eq(0).should('have.text', 'É campo obrigatório')
-        cy.contains('Cadastrar').click({force: true})
-        cy.get('input[name="passwordConfirmation"]').type('1234')
-        cy.contains('Cadastrar').click({force: true})
-        cy.get('#modalText').should('be.visible').contains('Nome não pode ser vazio.')
-        cy.get('#btnCloseModal').click()
-        cy.get('input[type="name"]').click({force: true}).should('be.enabled').type('Teste Automação')
-        cy.contains('Cadastrar').click({force: true})
-        cy.get('.styles__ContainerContent-sc-8zteav-1').should('be.visible')
-        cy.get('#modalText').contains('foi criada com sucesso')
-        cy.get('#btnCloseModal').click()
-})
-
-Cypress.Commands.add('cadastroCamposVazios', function() {
-    cy.get('.ihdmxA').click()
-    cy.get('input[name="email"]').eq(1).should('have.value', '')
-    cy.get('input[type="name"]').should('have.value', '')
-    cy.get('input[name="password"]').eq(1).should('have.value', '')
-    cy.get('input[name="passwordConfirmation"]').should('have.value','')
-})
-
-Cypress.Commands.add('cadastroConfimacaoDeSenhas', function() {
-    cy.get('.ihdmxA').click()
-    cy.get('input[name="email"]').eq(1).click({force: true}).type('teste@automacao.com')
-    cy.get('input[type="name"]').click({force: true}).should('be.enabled').type('Teste Automação')
-    cy.get('input[name="password"]').eq(1).click({force: true}).type('1234')
-    cy.get('input[name="passwordConfirmation"]').click({force: true}).type('12345')
-    cy.contains('Cadastrar').click({force: true})
-    cy.get('.styles__ContainerContent-sc-8zteav-1').should('be.visible')
-    cy.get('#modalText').contains('As senhas não são iguais.')
-    cy.get('#btnCloseModal').click()
-})
-
-Cypress.Commands.add('emailInvalido', function() {
-    cy.get('.ihdmxA').click()
-    cy.get('input[name="email"]').eq(1).type('teste', {force: true})
-    cy.get('.kOeYBn > .input__warging').should('have.text', 'Formato inválido')
-    cy.get('input[name="email"]').eq(1).clear().type('teste@', {force: true})
-    cy.get('.kOeYBn > .input__warging').should('have.text', 'Formato inválido')
-    cy.get('input[name="email"]').eq(1).clear().type('teste.com', {force: true})
-    cy.get('.kOeYBn > .input__warging').should('have.text', 'Formato inválido')
-    cy.get('input[name="email"]').eq(1).clear().type('teste@.com', {force: true})
-    cy.get('.kOeYBn > .input__warging').should('have.text', 'Formato inválido')
-    cy.get('input[name="email"]').eq(1).clear().type('com', {force: true})
-    cy.get('.kOeYBn > .input__warging').should('have.text', 'Formato inválido')
-    cy.get('input[name="email"]').eq(1).clear().type('@.com', {force: true})
-    cy.get('.kOeYBn > .input__warging').should('have.text', 'Formato inválido')
 })
